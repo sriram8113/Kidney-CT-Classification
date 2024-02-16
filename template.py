49ce93110347f43f869b106b09273eae6ad7e4c0
sriram <sriramarabelli@gmail.com>
2024-02-16 16:18:58 -0700

    made changes , added logger and chnaged name

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 #logging string
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = 'Kidney-CT-CNN-Classifier'
+project_name = 'cnnClassifier'
 
 list_of_files = [
     ".github/workflows/.gitkeep",
